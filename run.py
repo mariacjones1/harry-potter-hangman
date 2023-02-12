39533ed124c6793112f37daf022039e46ed899e8
mariacjones1 <mariacjones1@hotmail.co.uk>
2023-02-12 13:32:11 +0000

    Create function to show welcome message

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
 import random
 
 
+def welcome_message():
+    """
+    Prints welcome message and game instructions
+    """
+    print("Hello and welcome to...\n")
+    print("HARRY POTTER HANGMAN!\n")
+    print("Instructions:")
+    print("Test your knowledge of wizarding words.\n")
+    print("For each new round, choose a new letter you think is in the word.")
+    print("If you are correct, the letter will be added to the word.")
+    print("If you are incorrect, the man will be one step closer ")
+    print("to being hanged.\n")
+    print("You have five lives. If you use all five lives")
+    print("without guessing the word correctly, the man will be hanged and")
+    print("you will lose the game.\n")
+    print("Now that you know the rules, would you like to play?")
+
+
+welcome_message()
