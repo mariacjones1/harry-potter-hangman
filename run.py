a4cc0d15e69efe8b93231a546783fc86765779ee
mariacjones1 <mariacjones1@hotmail.co.uk>
2023-02-13 11:57:20 +0000

    Create loop to allow user to keep guessing as long as they still have lives left

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ def welcome_message():
           "If you are correct, the letter will be added to the word.\n"
           "If you are incorrect, the man will be one step closer "
           "to being hanged.\n"
-          "You have five lives. If you use all five lives"
+          "You have six lives. If you use all six lives"
           "without guessing the word correctly, the man will be hanged and"
           "you will lose the game.\n"
           "Now that you know the rules, would you like to play?")
@@ -50,32 +50,36 @@ def new_word(words):
     return word
 
 
-def guess_a_letter(word, blank_word):
+def play_game(word, blank_word):
     """
     Takes user input, checks it is a single letter that hasn't already
     been guessed, and checks if it is in the word
     """
-    guess = input("Guess a letter: ").upper()
     guessed_letters = []
+    lives = 6
 
-    if guess.isalpha() is False and len(guess) != 1:
-        print("Guess must be a single letter. Please guess again")
-    elif guess in guessed_letters:
-        print("You've already guessed that letter!")
-    elif guess in word:
-        print("Correct!")
-        guessed_letters.append(guess)
-        print(guessed_letters)
-        # following code adapted from
-        # https://www.youtube.com/watch?v=m4nEnsavl6w&ab_channel=Kite
-        word_as_list = list(blank_word.split(" "))
-        indices = [i for i, letter in enumerate(word) if letter == guess]
-        for index in indices:
-            word_as_list[index] = guess
-        blank_word = "".join(word_as_list)
-        print(blank_word)
-    else:
-        print("Incorrect :(")
+    while lives > 0:
+        guess = input("Guess a letter: ").upper()
+
+        if guess.isalpha() is False and len(guess) != 1:
+            print("Guess must be a single letter. Please guess again")
+        elif guess in guessed_letters:
+            print("You've already guessed that letter!")
+        elif guess in word:
+            print("Correct!")
+            guessed_letters.append(guess)
+            print(guessed_letters)
+            # following code adapted from
+            # https://www.youtube.com/watch?v=m4nEnsavl6w&ab_channel=Kite
+            word_as_list = list(blank_word)
+            indices = [i for i, letter in enumerate(word) if letter == guess]
+            for index in indices:
+                word_as_list[index] = guess
+            blank_word = "".join(word_as_list)
+            print(blank_word)
+        else:
+            print("Incorrect :(")
+            lives -= 1
 
 
 def main():
@@ -89,9 +93,9 @@ def main():
     word = new_word(words_to_guess)
     print(f"Word contains {len(word)} letters.")
     print(word)
-    blank_word = "_ " * len(word)
+    blank_word = "_" * len(word)
     print(blank_word)
-    guess_a_letter(word, blank_word)
+    play_game(word, blank_word)
 
 
 main()
